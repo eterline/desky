c3677fc595d3a3b1cbdf9105822c7b9b6010981e
Andrew <andrewalyuk@gmail.com>
2024-08-27 15:58:27 +0000

    added logging

--- cmd/desky/main.go
+++ cmd/desky/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/eterline/desky/internal/server"
 )
 
 func main() {
+	logPath := fmt.Sprintf(
+		"logs/desky_%v_%v_%v-%v_%v_%v.log",
+		time.Now().Year(), time.Now().Month(), time.Now().Day(),
+		time.Now().Hour(), time.Now().Minute(), time.Now().Second(),
+	)
+	file, _ := os.Create(logPath)
+	log.SetOutput(file)
+	defer file.Close()
+
 	go func() {
 		err := server.Start()
 		if err != nil {
